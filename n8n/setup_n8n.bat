283e08d95a9b24a9bd65a401b834ba720d8e9fc0
femto16 <kaz03301@gmail.com>
2025-08-27 15:53:15 +0900

    n8n　workflowとcredentialのバックアップワークフローの対応　マウントした n8n/backupフォルダに保存するように

--- n8n/setup_n8n.bat
+++ n8n/setup_n8n.bat
@@ -1,5 +1,8 @@
 pushd %~dp0docker
 
+mkdir ..\backup\workflow
+mkdir ..\backup\credential
+
 : encryption keyの生成
 for /f %%i in ('wsl openssl rand -hex 32') do (
     echo N8N_ENCRYPTION_KEY=%%i > .env
